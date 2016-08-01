675d6fcd31be6e8898ba6228172346acc0ffdf62
mcflugen <huttone@colorado.edu>
2016-08-01 12:36:20 -0600

    Changed to sort names using native byte values.

--- scripts/validate_names.sh
+++ scripts/validate_names.sh
@@ -3,4 +3,4 @@
 names_file=$1
 
 snvalidate $names_file || (echo "ERROR: $names_file contains bad names" && exit 1)
-sort $names_file | uniq | diff - $names_file || (echo "ERROR: $names_file is not sorted properly" && exit 2)
+LC_ALL=C sort --check $names_file || (echo "ERROR: $names_file is not sorted properly" && exit 2)
